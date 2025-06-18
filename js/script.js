d9f1c03dfd157051614f4be71755aa58a28f92a0
alisq <ali.shamas.qadeer@gmail.com>
2025-06-18 11:22:23 -0400

    ADDED CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-$("#center")
+const colHeights = [];
+
+document.querySelectorAll(".column").forEach(column => {
+  const interior = column.querySelector(".column_interior");
+  if (interior) {
+    colHeights.push(interior.offsetHeight);
+  }
+});
+
+const largest = Math.max(...colHeights);
+
+document.body.style.height = `${largest}px`;
+
+document.addEventListener("scroll", () => {
+  const scrollRatio = document.documentElement.scrollTop / (document.body.offsetHeight - window.innerHeight);
+
+  document.querySelectorAll(".column").forEach(column => {
+    const interior = column.querySelector(".column_interior");
+    if (interior) {
+      const targetScroll = (interior.offsetHeight + window.innerHeight) * scrollRatio;
+      column.scrollTop = targetScroll;
+    }
+  });
+});
